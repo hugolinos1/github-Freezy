45d2720ffae30349779650a9d7177a6eb95b04e6
hugolinos1 <hugues.rabier+1@gmail.com>
2024-11-12 13:41:55 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { Buffer } from 'buffer';
-globalThis.Buffer = Buffer;
+
+// Remplacer l'importation de Buffer par cette solution
+if (!globalThis.Buffer) {
+  globalThis.Buffer = require('buffer').Buffer;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
